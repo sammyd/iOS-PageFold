dd272d0b381acc6cbd920bc3775633d00c0d5354
Sam Davies <sdavies@shinobicontrols.com>
2014-05-22 22:28:05 +0100

    Added a simple spring animation to the page open and close

--- PageFold/RWPageFoldAnimator.m
+++ PageFold/RWPageFoldAnimator.m
@@ -7,10 +7,12 @@
 //
 
 #import "RWPageFoldAnimator.h"
+#import <POP/POP.h>
 
 @interface RWPageFoldAnimator ()
 
 @property (nonatomic, strong) RWPageFoldRenderer *renderer;
+@property (nonatomic, strong) POPAnimatableProperty *openProportionAnimateableProperty;
 
 @end
 
@@ -21,18 +23,48 @@
     self = [super init];
     if(self) {
         self.renderer = renderer;
+        self.openProportionAnimateableProperty =
+        [POPAnimatableProperty propertyWithName:@"com.raywenderlich.pagefoldanimator.openproportion"
+                                    initializer:^(POPMutableAnimatableProperty *prop) {
+             // read value
+             prop.readBlock = ^(RWPageFoldRenderer *r, CGFloat values[]) {
+                 values[0] = r.openProportion;
+             };
+             // write value
+             prop.writeBlock = ^(RWPageFoldRenderer *r, const CGFloat values[]) {
+                 r.openProportion = values[0];
+             };
+             // dynamics threshold
+             prop.threshold = 0.01;
+         }];
     }
     return self;
 }
 
 - (void)openPage
 {
-    self.renderer.openProportion = 0.0;
+    POPPropertyAnimation *anim = [self proportionAnimation];
+    anim.toValue = @0.0;
+    [self.renderer pop_addAnimation:anim forKey:@"OpenProportion"];
 }
 
 - (void)closePage
 {
-    self.renderer.openProportion = 0.45;
+    POPPropertyAnimation *anim = [self proportionAnimation];
+    anim.toValue = @0.45;
+    [self.renderer pop_addAnimation:anim forKey:@"OpenProportion"];
+}
+
+
+#pragma mark - Utility methods
+- (POPPropertyAnimation *)proportionAnimation
+{
+    POPSpringAnimation *anim = [POPSpringAnimation animation];
+    anim.property = self.openProportionAnimateableProperty;
+    anim.springBounciness = 12;
+    anim.springSpeed = 4;
+    anim.fromValue = @(self.renderer.openProportion);
+    return anim;
 }
 
 @end
